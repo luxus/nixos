a671858defac22727a928c7797ae233cb8c0ff16
luxus <luxuspur@gmail.com>
2024-12-02 20:36:00 +0100

    remove some spaces

--- modules/home/all/kde.nix
+++ modules/home/all/kde.nix
@@ -10,7 +10,7 @@
       wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Mountain/contents/images_dark/5120x2880.png";
     };
     kwin = {
-      virtualDesktops.number = 10;
+      virtualDesktops.number = 6;
       nightLight = {
         enable = true;
         location = {
@@ -32,28 +32,20 @@
         "Switch to Desktop 4" = "Meta+4";
         "Switch to Desktop 5" = "Meta+5";
         "Switch to Desktop 6" = "Meta+6";
-        "Switch to Desktop 7" = "Meta+7";
-        "Switch to Desktop 8" = "Meta+8";
-        "Switch to Desktop 9" = "Meta+9";
-        "Switch to Desktop 10" = "Meta+0";
         "Window to Desktop 1" = "Meta+Shift+1";
         "Window to Desktop 2" = "Meta+Shift+2";
         "Window to Desktop 3" = "Meta+Shift+3";
         "Window to Desktop 4" = "Meta+Shift+4";
         "Window to Desktop 5" = "Meta+Shift+5";
         "Window to Desktop 6" = "Meta+Shift+6";
-        "Window to Desktop 7" = "Meta+Shift+7";
-        "Window to Desktop 8" = "Meta+Shift+8";
-        "Window to Desktop 9" = "Meta+Shift+9";
-        "Window to Desktop 10" = "Meta+Shift+0";
         "Show Desktop" = "none";
       };
     };
     spectacle.shortcuts.launch = "Print";
-    hotkeys.commands."launch-alacritty" = {
-      name = "Launch alacritty";
+    hotkeys.commands."launch-ghostty" = {
+      name = "Launch ghostty";
       key = "Meta+Return";
-      command = "alacritty";
+      command = "ghostty";
     };
     hotkeys.commands."launch-krunner" = {
       name = "Launch krunner";

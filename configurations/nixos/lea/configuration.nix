aad1b23e58456bfcf294d024d2789b9e4b8a715f
luxus <luxuspur@gmail.com>
2025-02-15 21:44:48 +0100

    adding determite and making the systemd-boot remember last select until its merged

--- configurations/nixos/lea/configuration.nix
+++ configurations/nixos/lea/configuration.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   imports = [
     # Include the results of the hardware scan.
@@ -11,6 +12,15 @@
       enable = true;
       configurationLimit = 10; # Keep only the last 10 generations
       consoleMode = "auto";
+      extraInstallCommands = ''
+        default_cfg=$(${pkgs.coreutils}/bin/cat /boot/loader/loader.conf | ${pkgs.gnugrep}/bin/grep default | ${pkgs.gawk}/bin/awk '{print $2}')
+        tmp=$(${pkgs.coreutils}/bin/mktemp -d)
+
+        ${pkgs.coreutils}/bin/echo -ne "$default_cfg\0" | ${pkgs.iconv}/bin/iconv -f utf-8 -t utf-16le > $tmp/efivar.txt
+
+        ${pkgs.efivar}/bin/efivar -n 4a67b082-0a4c-41cf-b6c7-440b29bb8c4f-LoaderEntryLastBooted -w -f $tmp/efivar.txt
+        ${pkgs.systemd}/bin/bootctl set-default @saved
+      '';
     };
     efi = {
       canTouchEfiVariables = true;
